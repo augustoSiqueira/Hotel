1c53d4fbd42b4dc24a27648ca2af274bb8629d9f
augustoSiqueira <augusto.analista@gmail.com>
2014-05-02 19:20:38 -0300

    Criação do loginBean

---
+++ src/br/com/hotel/bean/LoginBean.java
@@ -0,0 +1,12 @@
+package br.com.hotel.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "loginBean")
+public class LoginBean implements Serializable {
+
+	private static final long serialVersionUID = 2969369454826694638L;
+
+}
